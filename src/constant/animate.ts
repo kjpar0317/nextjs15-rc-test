c85863fbb14e9afa3437ff77e4a864ef630f9670
박광주 <kjpar0317@innogrid.com>
2024-07-23 12:43:54 +0900

    variant typescript

--- src/constant/animate.ts
+++ src/constant/animate.ts
@@ -1,4 +1,43 @@
-export const ANIMATE_VARIANTS = {
+// 애니메이션 전환 옵션
+type TransitionOptions = Partial<{
+  type?: string; // 애니메이션의 타입 (e.g., "spring")
+  stiffness?: number; // 애니메이션의 경직도
+  damping?: number; // 애니메이션의 감쇠
+  duration?: number; // 애니메이션의 지속 시간
+  ease?: string; // 애니메이션의 이징 함수 (e.g., "easeInOut")
+  delay?: number; // 애니메이션의 지연 시간
+  origin?: number;
+}>;
+
+// Variant: 각 애니메이션 상태 (hidden, visible, exit)의 속성 정의
+type Variant = {
+  opacity?: number; // 투명도
+  width?: number | string;
+  height?: number | string;
+  filter?: string; // 필터 효과 (e.g., "blur(5px)")
+  scale?: number; // 크기 비율 (e.g., 1.5)
+  y?: number | string; // Y축 위치 (e.g., 100, "50%")
+  rotateY?: number; // Y축 회전 각도 (e.g., 180)
+  rotate?: number; // 회전 각도 (e.g., 45)
+  scaleX?: number; // X축 크기 비율
+  x?: number | string; // X축 위치 (e.g., 100, "50%")
+  transition?: TransitionOptions; // 전환 옵션
+};
+
+type FramerMotionVariant = {
+  init?: Variant;
+  animate?: Variant;
+  transition?: TransitionOptions;
+};
+
+export type ConstantFramerMotionVariant = {
+  expand: FramerMotionVariant;
+  onExpand: FramerMotionVariant;
+  longer: FramerMotionVariant;
+  action: FramerMotionVariant;
+};
+
+export const ANIMATE_VARIANTS: ConstantFramerMotionVariant = {
   expand: {
     init: { opacity: 0, scale: 0.5 },
     animate: { opacity: 1, scale: 1 },
